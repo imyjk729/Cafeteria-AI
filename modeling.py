2490c37d528d70f9625221d24a2cbaded38cfcf0
KyungHyun Jovinus Lee <lkh256@gmail.com>
2023-02-21 14:57:04 +0900

    refactor codes

--- modeling.py
+++ modeling.py
@@ -11,56 +11,105 @@ from dataset import Get_data
 def define_argparser():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--train_file", 
+    parser.add_argument(
+        "--train_file", 
         default='./data/train.csv', 
-        help="Train data path")
-    parser.add_argument("--test_file", 
+        help="Train data path"
+    )
+    parser.add_argument(
+        "--test_file", 
         default='./data/test.csv', 
-        help="Test data path")
+        help="Test data path"
+    )
 
-    parser.add_argument("--features_xgb", 
-        default=['본사정원수', '본사휴가자수', '본사출장자수', '현본사소속재택근무자수', '식사대상자',\
-                 '요일_lunch', '요일_dinner', '본사시간외근무명령서승인건수', 'covid',\
-                 'Year', 'Month', 'month_days_lunch', 'month_days_dinner'], 
-        nargs='*', type=list, help="Features list for xgboost")
-    parser.add_argument("--fixed_param_xgb", 
-        default={'objective':'reg:squarederror', 
-                 'eval_metric':'rmse', 
-                 'tree_method':'gpu_hist', 
-                 'gpu_id':'0'}, 
-        nargs='+', type=dict, help="Fixed parameter dictionary for xgboost")
-    parser.add_argument("--param_grid_xgb", 
-        default={'learning_rate':[0.5, 0.01, 0.001], 
-                 'max_depth':[3, 4, 5, 6], 
-                 'lambda':[1, 2 ,3], 
-                 'gamma':[0, 0.1, 0.2, 0.3]}, 
-        nargs='+', type=dict, help="Paramer dictionary to apply grid search for xgboost")
+    parser.add_argument(
+        "--features_xgb", 
+        default=[
+            '본사정원수', '본사휴가자수', '본사출장자수', '현본사소속재택근무자수', '식사대상자',\
+            '요일_lunch', '요일_dinner', '본사시간외근무명령서승인건수', 'covid',\
+            'Year', 'Month', 'month_days_lunch', 'month_days_dinner'
+        ], 
+        nargs='*', 
+        type=list, 
+        help="Features list for xgboost"
+    )
+    parser.add_argument(
+        "--fixed_param_xgb", 
+        default={
+            'objective':'reg:squarederror', 
+            'eval_metric':'rmse', 
+            'tree_method':'gpu_hist', 
+            'gpu_id':'0'
+        }, 
+        nargs='+', 
+        type=dict, 
+        help="Fixed parameter dictionary for xgboost"
+    )
+    parser.add_argument(
+        "--param_grid_xgb", 
+        default={
+            'learning_rate':[0.5, 0.01, 0.001], 
+            'max_depth':[3, 4, 5, 6], 
+            'lambda':[1, 2 ,3], 
+            'gamma':[0, 0.1, 0.2, 0.3]
+        }, 
+        nargs='+', 
+        type=dict, 
+        help="Paramer dictionary to apply grid search for xgboost"
+    )
 
-    parser.add_argument("--feature_lunch_model", 
+    parser.add_argument(
+        "--feature_lunch_model", 
         default=['식사대상자', '요일_lunch', '본사시간외근무명령서승인건수', 'covid'], 
-        nargs='*', type=list, help="Features list to use")
-    parser.add_argument("--feature_dinner_model", 
+        nargs='*', 
+        type=list, 
+        help="Features list to use"
+    )
+    parser.add_argument(
+        "--feature_dinner_model", 
         default=['식사대상자', '요일_dinner', '본사시간외근무명령서승인건수', 'covid'], 
-        nargs='*', type=list, help="Features list to use")
-    parser.add_argument("--models_list", 
+        nargs='*', 
+        type=list, 
+        help="Features list to use"
+    )
+    parser.add_argument(
+        "--models_list", 
         default=[RandomForestRegressor, LinearRegression, ElasticNet], 
-        nargs='*', type=list, help="Models list")
-      
-    parser.add_argument("--rf_param", 
-        default={'n_jobs':[-1], 
-                 'n_estimators':[10, 50, 100, 150, 200, 250, 300, 350, 400], 
-                 'max_depth':[2, 3, 4, None]}, 
-        nargs='+', type=dict, help="Dictionary of RandomForest regression parameters")
-    parser.add_argument("--linear_param", 
-        default={'fit_intercept':[True, False], 
-                 'normalize':[True, False]}, 
-        nargs='+', type=dict, help="Dictionary of Linear regression parameters")
-    parser.add_argument("--elastic_param", 
-        default={'alpha':range(0, 2, 20), 
-                 'l1_ratio':range(0, 1, 10), 
-                 'normalize':[True, False], 
-                 'fit_intercept':[True, False]},
-        nargs='+', type=dict, help="Dictionary of ElasticNet parameters")
+        nargs='*', type=list, help="Models list"
+    )
+    parser.add_argument(
+        "--rf_param", 
+        default={
+            'n_jobs':[-1], 
+            'n_estimators':[10, 50, 100, 150, 200, 250, 300, 350, 400], 
+            'max_depth':[2, 3, 4, None]
+        }, 
+        nargs='+', 
+        type=dict, 
+        help="Dictionary of RandomForest regression parameters"
+    )
+    parser.add_argument(
+        "--linear_param", 
+        default={
+            'fit_intercept':[True, False], 
+            'normalize':[True, False]
+        }, 
+        nargs='+', 
+        type=dict, 
+        help="Dictionary of Linear regression parameters"
+    )
+    parser.add_argument(
+        "--elastic_param", 
+        default={
+            'alpha':range(0, 2, 20), 
+            'l1_ratio':range(0, 1, 10), 
+            'normalize':[True, False], 
+            'fit_intercept':[True, False]
+        },
+        nargs='+', 
+        type=dict, 
+        help="Dictionary of ElasticNet parameters"
+    )
 
     # Set parameters
     args = parser.parse_args()
@@ -77,61 +126,115 @@ def main(args):
     train_data, test_data = get_data.preprocess(train_df, test_df)
     train_data, test_data = get_data.feature_engineering(train_data, test_data)
 
-    xgboost_results_lunch = grid_search(train_data=train_data,
-                                        parameter_candidate=args.param_grid_xgb, 
-                                        model_name=xgboost,
-                                        basic_param=args.fixed_param_xgb, 
-                                        n_splits=5, 
-                                        feature_mask=args.features_xgb, 
-                                        label='중식계')
+    xgboost_results_lunch = grid_search(
+        train_data=train_data,
+        parameter_candidate=args.param_grid_xgb, 
+        model_name=xgboost,
+        basic_param=args.fixed_param_xgb, 
+        n_splits=5, 
+        feature_mask=args.features_xgb, 
+        label='중식계'
+    )
     
-    xgboost_results_dinner = grid_search(train_data=train_data,
-                                        parameter_candidate=args.param_grid_xgb, 
-                                        model_name=xgboost,
-                                        basic_param=args.fixed_param_xgb, 
-                                        n_splits=5, 
-                                        feature_mask=args.features_xgb, 
-                                        label='석식계')
+    xgboost_results_dinner = grid_search(
+        train_data=train_data,
+        parameter_candidate=args.param_grid_xgb, 
+        model_name=xgboost,
+        basic_param=args.fixed_param_xgb, 
+        n_splits=5, 
+        feature_mask=args.features_xgb, 
+        label='석식계'
+    )
 
-    xgboost_lunch_results = xgboost_results_lunch.groupby(['parameter'])['loss'].apply(np.mean).reset_index(name='loss')
-    xgboost_lunch_results['type'] = 'lunch'
-    xgboost_lunch_results['model'] = xgboost.__name__
+    xgboost_lunch_results = (
+        xgboost_results_lunch
+        .groupby(['parameter'])['loss']
+        .apply(np.mean)
+        .reset_index(name='loss')
+        .assign(
+            type = 'lunch',
+            model = xgboost.__name__,
+        )
+    )
 
 
-    xgboost_dinner_results = xgboost_results_dinner.groupby(['parameter'])['loss'].apply(np.mean).reset_index(name='loss')
-    xgboost_dinner_results['type'] = 'dinner'
-    xgboost_dinner_results['model'] = xgboost.__name__
+    xgboost_dinner_results = (
+        xgboost_results_dinner
+        .groupby(['parameter'])['loss']
+        .apply(np.mean)
+        .reset_index(name='loss')
+        .assign(
+            type = 'dinner',
+            model = xgboost.__name__,
+        )
+    )
 
-    results = pd.concat([xgboost_lunch_results, xgboost_dinner_results], axis=0)
+    results = pd.concat(
+        [
+            xgboost_lunch_results, 
+            xgboost_dinner_results
+        ], 
+        axis=0
+    )
 
     # Apply grid search and proceed with modeling.
-    parameters_list = [args.rf_param, args.linear_param, args.elastic_param]
+    parameters_list = [
+        args.rf_param, 
+        args.linear_param, 
+        args.elastic_param
+    ]
 
     for i in range(len(args.models_list)):
-        results_lunch = grid_search(train_data=train_data,
-                                   parameter_candidate=parameters_list[i],
-                                   model_name=args.models_list[i],
-                                   basic_param=args.fixed_param_xgb,
-                                   n_splits=5,
-                                   feature_mask=args.feature_lunch_model, 
-                                   label='중식계')
-        results_dinner = grid_search(train_data=train_data,
-                                    parameter_candidate=parameters_list[i],
-                                    model_name=args.models_list[i],
-                                    basic_param=args.fixed_param_xgb,
-                                    n_splits=5,
-                                    feature_mask=args.feature_dinner_model, 
-                                    label='석식계')
+        results_lunch = grid_search(
+            train_data=train_data,
+            parameter_candidate=parameters_list[i],
+            model_name=args.models_list[i],
+            basic_param=args.fixed_param_xgb,
+            n_splits=5,
+            feature_mask=args.feature_lunch_model, 
+            label='중식계'
+        )
+        
+        results_dinner = grid_search(
+            train_data=train_data,
+            parameter_candidate=parameters_list[i],
+            model_name=args.models_list[i],
+            basic_param=args.fixed_param_xgb,
+            n_splits=5,
+            feature_mask=args.feature_dinner_model, 
+            label='석식계'
+        )
 
-        lunch_results = results_lunch.groupby(['parameter'])['loss'].apply(np.mean).reset_index(name='loss')
-        lunch_results['type'] = 'lunch'
-        lunch_results['model'] = args.models_list[i].__name__
+        lunch_results = (
+            results_lunch
+            .groupby(['parameter'])['loss']
+            .apply(np.mean)
+            .reset_index(name='loss')
+            .assign(
+                type = 'lunch',
+                model = args.models_list[i].__name__,
+            )
+        )
 
-        dinner_results = results_dinner.groupby(['parameter'])['loss'].apply(np.mean).reset_index(name='loss')
-        dinner_results['type'] = 'dinner'
-        dinner_results['model'] = args.models_list[i].__name__
+        dinner_results = (
+            results_dinner
+            .groupby(['parameter'])['loss']
+            .apply(np.mean)
+            .reset_index(name='loss')
+            .assign(
+                type = 'dinner',
+                model = args.models_list[i].__name__
+            )
+        )
 
-        results = pd.concat([results, lunch_results, dinner_results], axis=0)
+        results = pd.concat(
+            [
+                results, 
+                lunch_results, 
+                dinner_results
+            ], 
+            axis=0
+        )
 
     results.to_excel("./ml_results.xlsx", index=False)
 
--- submit.py
+++ submit.py
@@ -11,41 +11,65 @@ from sklearn.metrics import mean_absolute_error
 def define_argparser():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--train_file", 
-        default='./data/train.csv', 
-        help="Train data path")
-    parser.add_argument("--test_file", 
-        default='./data/test.csv', 
-        help="Test data path")
-    parser.add_argument("--out_file", 
-        default='./data/sample_submission.csv', 
-        help="output file path")
+    parser.add_argument(
+        "--train_file",
+        default='./data/train.csv',
+        help="Train data path"
+    )
+    parser.add_argument(
+        "--test_file",
+        default='./data/test.csv',
+        help="Test data path"
+    )
+    parser.add_argument(
+        "--out_file",
+        default='./data/sample_submission.csv',
+        help="output file path"
+    )
 
-    parser.add_argument("--feature_mask", 
-        default=['본사정원수', '본사휴가자수', '본사출장자수', '현본사소속재택근무자수',\
-                 '식사대상자', '요일_lunch', '요일_dinner', '본사시간외근무명령서승인건수', 'covid'], 
-        nargs='*', type=list, help="Features list to use")
-    parser.add_argument("--param_lunch", 
-        default={'learning_rate':0.01, 
-                 'max_depth':3, 
-                 'lambda':1, 
-                 'gamma':0, 
-                 'objective':'reg:squarederror', 
-                 'eval_metric':'rmse', 
-                 'tree_method':'gpu_hist', 
-                 'gpu_id':'0'}, 
-        nargs='+', type=dict, help="Paramer dictionary for lunch")
-    parser.add_argument("--param_dinner", 
-        default={'learning_rate':0.01, 
-                 'max_depth':4, 
-                 'lambda':2, 
-                 'gamma':0, 
-                 'objective':'reg:squarederror', 
-                 'eval_metric':'rmse', 
-                 'tree_method':'gpu_hist', 
-                 'gpu_id':'0'}, 
-        nargs='+', type=dict, help="Paramer dictionary for dinner")
-    
+    parser.add_argument(
+        "--feature_mask",
+        default=[
+            '본사정원수', '본사휴가자수', '본사출장자수', '현본사소속재택근무자수',
+            '식사대상자', '요일_lunch', '요일_dinner', '본사시간외근무명령서승인건수', 'covid'
+        ],
+        nargs='*',
+        type=list,
+        help="Features list to use"
+    )
+    parser.add_argument(
+        "--param_lunch",
+        default={
+            'learning_rate': 0.01,
+            'max_depth': 3,
+            'lambda': 1,
+            'gamma': 0,
+            'objective': 'reg:squarederror',
+            'eval_metric': 'rmse',
+            'tree_method': 'gpu_hist',
+            'gpu_id': '0'
+        },
+        nargs='+',
+        type=dict,
+        help="Paramer dictionary for lunch"
+    )
+    parser.add_argument(
+        "--param_dinner",
+        default={
+            'learning_rate': 0.01,
+            'max_depth': 4,
+            'lambda': 2,
+            'gamma': 0,
+            'objective': 'reg:squarederror',
+            'eval_metric': 'rmse',
+            'tree_method': 'gpu_hist',
+            'gpu_id': '0'
+        },
+        nargs='+',
+        type=dict,
+        help="Paramer dictionary for dinner"
+    )
+
     # Set parameters
     args = parser.parse_args()
 
@@ -62,22 +86,49 @@ def main(args):
     df_train, df_test = get_data.preprocess(train_df, test_df)
     df_train, df_test = get_data.feature_engineering(df_train, df_test)
 
-    trainset, testset = train_test_split(df_train, train_size=0.8, random_state=1004)
-    
+    trainset, testset = train_test_split(
+        df_train, train_size=0.8, random_state=1004)
+
     # Modeling
-    dtrain_lunch = xgb.DMatrix(trainset[args.feature_mask], label=trainset['중식계'])
-    dtest_lunch = xgb.DMatrix(testset[args.feature_mask], label=testset['중식계'])
-    xgb_model_lunch = xgb.train(dtrain=dtrain_lunch, params=args.param_lunch, evals=[(dtest_lunch, 'validation')], 
-                                verbose_eval=0, early_stopping_rounds=500, num_boost_round=2000)
-    
-    dtrain_dinner = xgb.DMatrix(trainset[trainset['석식계'] != 0][args.feature_mask], label=trainset[trainset['석식계'] != 0]['석식계'])
-    dtest_dinner = xgb.DMatrix(testset[testset['석식계'] != 0][args.feature_mask], label=testset[testset['석식계'] != 0]['석식계'])
-    xgb_model_dinner = xgb.train(dtrain=dtrain_dinner, params=args.param_dinner, evals=[(dtest_dinner, 'validation')], 
-                                 verbose_eval=0, early_stopping_rounds=500, num_boost_round=2000)
+    dtrain_lunch = xgb.DMatrix(
+        trainset[args.feature_mask],
+        label=trainset['중식계']
+    )
+    dtest_lunch = xgb.DMatrix(
+        testset[args.feature_mask],
+        label=testset['중식계']
+    )
+
+    xgb_model_lunch = xgb.train(
+        dtrain=dtrain_lunch,
+        params=args.param_lunch,
+        evals=[(dtest_lunch, 'validation')],
+        verbose_eval=0,
+        early_stopping_rounds=500,
+        num_boost_round=2000
+    )
+
+    dtrain_dinner = xgb.DMatrix(
+        trainset[trainset['석식계'] != 0][args.feature_mask],
+        label=trainset[trainset['석식계'] != 0]['석식계']
+    )
+    dtest_dinner = xgb.DMatrix(
+        testset[testset['석식계'] != 0][args.feature_mask],
+        label=testset[testset['석식계'] != 0]['석식계']
+    )
+
+    xgb_model_dinner = xgb.train(
+        dtrain=dtrain_dinner,
+        params=args.param_dinner,
+        evals=[(dtest_dinner, 'validation')],
+        verbose_eval=0,
+        early_stopping_rounds=500,
+        num_boost_round=2000
+    )
 
     lunch = xgb_model_lunch.predict(xgb.DMatrix(df_test[args.feature_mask]))
     dinner = xgb_model_dinner.predict(xgb.DMatrix(df_test[args.feature_mask]))
-    
+
     submission['중식계'] = lunch
     submission['석식계'] = dinner
 
@@ -87,10 +138,3 @@ def main(args):
 if __name__ == "__main__":
     args = define_argparser()
     main(args)
-
-
-    
-
-    
-
-
